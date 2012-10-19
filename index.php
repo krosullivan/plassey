6a856b83e1e0d343b1ad56e398503d167de251d1
Kevin O Sullivan <krosullivan@gmail.com>
2012-10-19 21:51:37 +0100

    Test

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once('includes/document_head.php');?>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <?php require_once('includes/nav.php');?>
+
+        <?php require_once('includes/container.php')?>
+
+        <?php require_once('includes/scripts.php')?>
+    </body>
+</html>
